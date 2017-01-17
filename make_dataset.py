6cec7ee9f52fe404251755afe32972ba14c26e38
syvokobyla <svitlana.syvokon@gmail.com>
2017-01-17 13:59:54 +0200

    Update on data preparing

--- make_dataset.py
+++ make_dataset.py
@@ -1,40 +1,72 @@
 import librosa
 import numpy as np
 
-def prepare_audio_files():
+def prepare_audio_files(paths):
+    # paths = list((vocal_file, instrument_file, mix_file))
     target_rate = 16384
-    print("Resample mix")
-    mix, rate = librosa.load("mix.wav", sr=target_rate)
-    print("SFFT")
-    mix_freqs = librosa.core.stft(mix)
-    np.save("mix.sfft", mix_freqs)
+    print("Resample files")
+    files_mix_freqs = []
+    files_vocal_freqs = []
+    for vocal_file, mix_file in paths:
+        mix, rate = librosa.load(mix_file, sr=target_rate)
+        vocal, rate = librosa.load(vocal_file, sr=target_rate)
 
-def collect_samples():
+        print("STFT")
+        mix_freqs = np.stack(librosa.core.stft(mix), axis=1) # (..., 1025)
+        vocal_freqs = np.stack(librosa.core.stft(vocal), axis=1)
+
+        concat = lambda x, y: np.concatenate((x, y), axis = 0 if x else x = y
+
+        files_vocal_freqs = concat(files_vocal_freqs, vocal_freqs)
+        files_mix_freqs = concat(files_mix_freqs, mix_freqs)
+
+    return files_mix_freqs,
+
+    np.save("mix.stft", files_mix_freqs)
+    np.save("vocal.stft", files_vocal_freqs)
+
+def prepare_freq_file(paths, freq_file_path):
+        target_rate = 16384
+        print("Resample files")
+        files_freqs = []
+        for f in paths:
+            sample, rate = librosa.load(f, sr=target_rate)
+            print("STFT")
+            freqs = np.stack(librosa.core.stft(sample), axis=1) # (..., 1025)
+
+            if not files_freqs:
+                files_freqs = np.concatenate((files_freqs, freqs), axis = 0)
+            else:
+                files_freqs = freqs
+
+        np.save(freq_file_path, files_freqs)
+
+def collect_samples(x_file_name, y_file_name):
     """Save X and Y samples to files. """
 
-    window_size = 2048
-    examples_wanted = 1000
-    examples_wanted_vocals = examples_wanted // 2
-    examples_wanted_non_vocal = examples_wanted - examples_wanted_vocals
+    #window_size = 2048
+    #examples_wanted = 1000
+    #examples_wanted_vocals = examples_wanted // 2
+    #examples_wanted_non_vocal = examples_wanted - examples_wanted_vocals
 
-    print("Load resampled .wav files")
-    x = np.load('mix-16k.pickle.npy')
-    y = np.load('vocals-16k.pickle.npy')
-    assert len(x) == len(y), "mix and vocals length don't match"
+    print("Load resampled stft files")
+    x = np.load(x_file_name) #'mix.stft.npy')
+    y = np.load(y_file_name) #'vocal.stft.npy')
+    assert len(x) == len(y), "mix,vocal and instrument lengths don't match"
 
-    print("Generate frequency frames")
-    x = librosa.core.stft(x)
-    y = librosa.core.stft(y)
 
     print("Pick samples")
     collected_samples = 0
     rng = np.random.RandomState(0)
     X = []
     Y = []
+    examples_wanted = len(x)
     while collected_samples < examples_wanted:
         pos = rng.randint(len(x))
-        sample_x = x[:,pos]
-        sample_y = y[:,pos]
+        #sample_x = x[:,pos]
+        sample_x = x[pos]
+        #sample_y = y[:,pos]
+        sample_y = y[pos]
         Y.append(sample_y)
         X.append(sample_x)
         collected_samples += 1
@@ -44,6 +76,8 @@ def collect_samples():
     # Throw away phase, only keep magnitude
     X = np.stack(np.abs(X))
     Y = np.stack(np.abs(Y))
+
+    print(X.shape)
     np.save("X", X)
     np.save("Y", Y)
 
@@ -51,6 +85,65 @@ def collect_samples():
     print("X.shape = {}".format(X.shape))
     print("Y.shape = {}".format(Y.shape))
 
+def get_dataset_paths(audio_folder):
+    import glob, os, yaml
+    #audio_folder = 'MedleyDB_sample/Audio'
+    for filename in glob.glob(audio_folder + '/*/*.yaml'):
+        metadata = yaml.load(open(filename))
+        mix_filename = metadata["mix_filename"]
+        vocal_filenames = [
+                x['filename'] for x in metadata['stems'].values()
+                if 'singer' in x['instrument']]
+        if vocal_filenames:
+            vocal_path = glob.glob(audio_folder +'/*/*/' + vocal_filenames[0])
+            mix_path = glob.glob(audio_folder +'/*/' + mix_filename)
+            yield vocal_path[0], mix_path[0]
+
+def get_track_paths(audio_folder):
+    import glob, os, yaml
+    #audio_folder = 'MedleyDB_sample/Audio'
+    for filename in glob.glob(audio_folder + '/*/*.yaml'):
+        metadata = yaml.load(open(filename))
+        mix_filename = metadata["mix_filename"]
+        vocal_filenames = [
+                x['filename'] for x in metadata['stems'].values()
+                if 'singer' in x['instrument']]
+        instruments_filenames = [
+                x['filename'] for x in metadata['stems'].values()
+                if 'singer' not in x['instrument']]
+
+        if vocal_filenames:
+            vocal_paths = []
+            instruments_paths = []
+            for f in vocal_filenames:
+                vocal_paths += glob.glob(audio_folder +'/*/*/' + f)
+            for f in instruments_filenames:
+                instruments_paths += glob.glob(audio_folder +'/*/*/' + f)
+
+            #mix_path = glob.glob(audio_folder +'/*/' + mix_filename)
+
+            yield vocal_paths, instruments_paths
+
+def mix_wav_files(paths):
+    return path
+
 if __name__ == '__main__':
-    prepare_audio_files()
-    #collect_samples()
+    vocal_paths, instruments_paths = get_track_paths('MedleyDB_sample/Test_Audio')
+    vocal_concat_paths = [mix_wav_files(vocal_stems_paths) for vocal_stems_paths in vocal_paths]
+    instruments_concat_paths = [mix_wav_files(instruments_stems_paths) for instruments_stems_paths in instruments_paths]
+    mix_concat_paths = [mix_wav_files(paths) for paths in zip(vocal_concat_paths, instruments_concat_paths)]
+
+    #prepare_audio_files(zip(vocal_concat_paths, mix_concat_paths))
+    prepare_freq_file(vocal_concat_paths, "vocal.stft")
+    prepare_freq_file(mix_concat_paths, "mix.stft")
+    collect_samples('mix.stft.npy', 'vocal.stft.npy')
+
+    #TODO: prepare test vocal and mix freqs files
+    vocal_test_paths = vocal_concat_paths
+    mix_test_paths = mix_concat_paths
+    prepare_freq_file(vocal_test_paths, "test_vocal.stft")
+    prepare_freq_file(mix_test_paths, "test_mix.stft")
+
+
+
+    #prepare_audio_files(get_dataset_paths('MedleyDB_sample/Test_Audio'))
--- svitlana_model.py
+++ svitlana_model.py
@@ -3,7 +3,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 import pickle
 import numpy as np
-import librosa 
+import librosa
+import mir_eval.separation
 
 def build_model():
     model = Sequential([
@@ -18,29 +19,91 @@ def build_model():
 def fit(model):
     X = np.load("X.npy")
     Y = np.load("Y.npy")
-
+    print(X.shape)
     model.fit(X, Y, batch_size=32, nb_epoch=3)
     return model
 
-def predict(model):
+def predict(model, mix_freqs):
 
-    mix = np.load("mix.sfft.npy").transpose()
-    X = np.abs(mix)
+    #mix = np.load("mix.stft.npy")#.transpose()
+    X = np.abs(mix_freqs)
     num_examples, num_features = X.shape
     Y = model.predict(X, verbose=True)
     Y = np.minimum(Y, X)
 
     # Restore phase
-    Y_complex = np.exp(1j * np.angle(mix))
+    Y_complex = np.exp(1j * np.angle(mix_freqs))
     Y_complex *= Y
 
-    result = librosa.core.istft(Y_complex.transpose())
-    rate = 16384
-    librosa.output.write_wav("predicted.wav", result, sr=rate)
+    return Y_complex
+
+    #result = librosa.core.istft(Y_complex.transpose())
+    #rate = 16384
+    #librosa.output.write_wav("predicted.wav", result, sr=rate)
+
+    #np.save("predicted", result)
+
+    #source_vocal = librosa.core.istft(np.load("vocal.stft.npy").transpose())
+    #mix = librosa.core.istft(np.load("mix.stft.npy").transpose())
+
+    #librosa.output.write_wav("temp_source.wav", source_vocal, sr=rate)
+    #print(source_vocal.shape, result.shape)
+
+
+
+def evaluate():
+    #source_vocal = librosa.core.istft(np.load("vocal.stft.npy").transpose()) #wRONG!!!!!!!!
+    predicted_vocal = np.load("predicted.npy")
+    predicted_instrument =
+    vocal =
+    instrument =
+    #mix = librosa.core.istft(np.load("mix.stft.npy").transpose())
+
+
+    #mir_eval.separation.validate(source, res)
+    mir_eval.separation.bss_eval_sources([vocal, instrument], [predicted_vocal, predicted_instrument])
+
+    #mir_eval.separation.validate(np.array([source_vocal]), np.array([result]))
+        #(sdr, sir, sar, perm) = mir_eval.separation.bss_eval_sources(source_vocal, result)
+    #print(sdr, sir, sar, perm)
+    #(array([-0.85175804, -0.85175804]), array([ 252.13302926,  252.13302926]), array([-0.85175804, -0.85175804]), array([0, 1]))
+    #[-0.85175804] [ inf] [-0.85175804] [0] - mix vs vocal
+    #[ 2.61667555] [ inf] [ 2.61667555] [0] - source vocal vs result
+    #[ 269.04395712] [ inf] [ 269.04395712] [0] - siurce vocal vs source vocal
+
+    # source vs rsult (array([ 2.61667555,  2.61667555]), array([ 223.19718976,  223.19718976]), array([ 2.61667555,  2.61667555]), array([0, 1]))
+    #source vs source([ 269.04395712,  269.04395712]), array([ 266.65785807,  266.65785807]), array([ 263.6174841,  263.6174841]), array([0, 1]))
+
+
+def _any_source_silent(sources):
+    """Returns true if the parameter sources has any silent first dimensions"""
+    return np.any(np.all(np.sum(
+        sources, axis=tuple(range(2, sources.ndim))) == 0, axis=1))
+
+
 
 model = build_model()
 model = fit(model)
-    #model.load_weights("model_weights.keras")
-model.save_weights("model_weights.keras")
-predict(model)
 
+mix_freqs = np.load("test_mix.stft.npy")
+predicted_vocal_freqs = predict(model, mix_freqs)
+
+predicted_vocal_samples = librosa.core.istft(predicted_vocal_freqs.transpose())
+rate = 16384
+librosa.output.write_wav("predicted.wav", predicted_vocal_samples, sr=rate)
+
+predicted_instrument_samples =
+
+source_vocal =
+source_instrument =
+
+sdr, sir, sar, perm = mir_eval.separation.bss_eval_sources(
+                    [vocal, instrument],
+                    [predicted_vocal_samples, predicted_instrument_samples]
+                    )
+
+
+#model.load_weights("model_weights.keras")
+#model.save_weights("model_weights.keras")
+#predict(model)
+#evaluate()
